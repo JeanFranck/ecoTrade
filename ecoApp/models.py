3be0edb3c9700f089d67e4b384407dc25fa1b697
JeanFranck <A20206314@pucp.edu.pe>
2024-09-20 01:59:32 -0500

    Se agrego la -seccion- a models y a views

--- ecoApp/models.py
+++ ecoApp/models.py
@@ -10,6 +10,6 @@ class grado (models.Model):
     nivelRelacionado = models.ForeignKey(nivel, null=True, blank=True, on_delete=models.SET_NULL)
 
 class seccion (models.Model):
-    nombreGrado = models.CharField(max_length=48, null=True, blank=True)
+    nombreSeccion = models.CharField(max_length=48, null=True, blank=True)
     nivelRelacionado = models.ForeignKey(nivel, null=True, blank=True, on_delete=models.SET_NULL)
     gradoRelacionado = models.ForeignKey(grado, null=True, blank=True, on_delete=models.SET_NULL)
